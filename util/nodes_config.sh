56ea7ee044fa23907a80efa8023f87e86b99d607
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2013-04-03 23:21:21 -0700

    added nodes.json configuration script and created util directory and put openshift installer inside

---
+++ util/nodes_config.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+##
+##  Strava.node.js Nodes Configuration (Bash) Installer
+##  This will overwrite an existing nodes.json
+##  Ronaldo Barbachano April 2013
+filename=$1
+
+if [ "$filename" == '' ]; then
+    echo 'Missing argument (filename)'
+    exit
+fi
+
+
+if [ -f "$filename" ]; then
+    echo 'File exists'
+    exit
+fi
+
+ip=
+while [ -z $ip ]
+    do
+        echo -n 'Your local Ip address ( 192.168.0.1 ) ?
+'
+        read ip
+    done
+    
+while [ -z $port_ipaddress ]
+    do
+        echo -n 'Your local port for master node? ( 8080 ) ?
+'
+        read port_ipaddress
+    done
+port_segment=
+while [ -z $port_segment ]
+    do
+        echo -n 'Your local port for segment node? ( 8081 ) ?
+'
+        read port_segment
+    done
+## ensure port segment and ip address are not the same (as strings)
+if [ "$port_ipaddress" == "$port_segment" ]; then
+    echo 'Segment port is already in use for the master node.'
+    exit
+fi
+
+port_rides=
+while [ -z $port_rides ]
+    do
+        echo -n 'Your local port for rides node? ( 8082 ) ?
+'
+        read port_rides
+    done
+
+if [ "$port_rides" == "$port_segment" ] || [ "$port_rides" == "$port_ipaddress" ]; then
+    echo 'Rides port is already in use'
+    exit
+fi
+
+## TIMEOUTS (in seconds)
+echo '*****
+Timeouts
+*****
+
+Values are in seconds.
+
+'
+to_rides=
+while [ -z $to_rides ]
+    do
+        echo -n 'Timeout (seconds) for rides (9600) 
+'
+        read to_rides
+    done
+to_effort=
+while [ -z $to_effort ]
+    do
+        echo -n 'Timeout (seconds) for ride efforts (25200)
+'
+        read to_effort
+    done
+to_segment_best=
+while [ -z $to_segment_best ]
+    do
+        echo -n 'Timeout (seconds) for segment efforts best (9600)
+'
+        read to_segment_best
+    done
+to_club_best=
+while [ -z $to_club_best ]
+    do
+        echo -n 'Timeout (seconds) for segment efforts best by club (9600)
+'
+        read to_club_best
+    done
+    
+    rhc_domain=
+    while [ -z $rhc_domain ]
+    do
+        echo -n 'Redhat openshift namespace (optional)?
+'
+        read rhc_domain
+    done
+
+        
+echo '{ "local":{
+"ipaddress" : "'$ip'",
+"segment" : "'$ip':'$port_segment'",
+"rides" : "'$ip':'$port_rides'",
+"_ports" : {"ipaddress" : '$port_ipaddress', "segment": '$port_segment',"rides":'$port_rides'},
+"_timeouts" : {"athlete+rides_offset":'$to_rides',"efforts_timeout":'$to_effort',"club_segment":'$to_club_best',"segment":'$to_segment_best'}
+},
+"openshift":
+{
+"_oDomain" : "'$rhc_domain'",
+"_cDomain" : "rhcloud.com"
+}
+}
+'> "$filename"
---
+++ util/openshift_installer.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+
+##
+##  Strava.node.js Openshift (Bash) Installer
+##
+##  Ronaldo Barbachano (April 2013)
+##  This requires that you have rhc client tools and git
+##  installed and your client configured.
+##  Refer to redhat's openshift documentation to do this.
+##
+##  This script will create an redhat cloud app, and will install the
+##  appropriate Strava.node.js branch. It also allows
+##  for installation of any version of node (optional) through onscreen prompts.
+##  In addition it'll push the changes to your repo so if its
+##  completely sucessful it'll be online by the time the script exits.
+##
+##  Basic Use
+##
+##  (make sure script has execute permissions 755 or better)
+##  Place/launch from folder where you don't mind a repo being created!
+##  ./openshift_installer
+##  (Follow onscreen instructions)
+##
+##  Use With Arguments
+##
+##  ./openshift_installer.sh master
+##  ./openshift_installer.sh rides
+##  ./openshift_installer.sh segment
+##
+##  Notes
+##
+##  The 'master' node has an few extra options and does
+##  not have to have the same rhcloud namespace as the others,
+##  but should refer to a single namespace that rides and segment exist in.
+##
+##  Each node reads from an independent nodes.json file but only the master
+##  server really 'uses' this information - to communicate with
+##  rides/segment nodes otherwise its only used for cache settings which
+##  need to be configured individually.
+##
+
+function exitStatus {
+## used to end program flow if specific tasks (commands) fail
+    if [ "$1" != 0 ]; then
+        exit
+    fi
+}
+
+function createApp {
+    ## createApp name
+    rhc app create "$1" nodejs-0.6
+    exitStatus $?
+}
+
+function dirCheck {
+    if [ -d "$1" ]; then
+        echo 'A directory with the same name as the app already exists, please remove it.'
+        exit
+    fi
+}
+
+## openshift NODE_TYPE
+rhc_name=$1
+
+clear
+
+echo -n '**********
+Welcome to the Strava.nodes.js Openshift Helper Script
+*********
+
+
+'
+
+if [ "$1" == '' ]; then
+    rhc_name=
+    while [ -z $rhc_name ]
+    do
+        echo -n 'What node server would you like to create? (master/rides/segment)?
+    '
+        read rhc_name
+    done
+elif [ "$1" == 'master' ] || [ "$1" == 'rides' ] || [ "$1" == 'segment' ]; then
+    echo -n 'Creating node ' $1 '
+'
+else
+    echo 'Invalid argument.
+    Did you mean?
+    openshift.sh master
+    openshift.sh master <name>
+    openshift.sh rides
+    openshift.sh segment
+    '
+fi
+## could glean this from rhc stuff..
+if [ "$rhc_name" == 'master' ] || [ "$rhc_name" == 'segment' ] || [ "$rhc_name" == 'rides' ]; then
+    ## run rhc server and hope exit status returns false if the server isn't running fine? but this should be
+    ## more of a installation check than anything else
+    rhc server
+    exitStatus $?
+
+    if [ "$rhc_name" == 'master' ]; then
+
+        custom_app_name_arg=$2
+
+        if [ "$custom_app_name_arg" == '' ]; then
+            custom_app_name=
+            while [ -z $custom_app_name ]
+            do
+                echo 'Pick a name for the master app
+                '
+                read custom_app_name
+            done
+        else
+            custom_app_name="$2"
+        fi
+        ## Needed to do it this way to properly fetch the data from Strava.node.js without too much modification
+        
+        dirCheck "$custom_app_name"
+        createApp $custom_app_name
+        cd $custom_app_name
+    else
+        dirCheck $rhc_name
+        createApp $rhc_name
+        cd $rhc_name
+
+    fi
+
+    custom_nodejs=
+    while [ -z $custom_nodejs ]
+    do
+        echo -n 'Would you like to install a custom version of Node.js? 
+     This will increase your next push time
+    (y/n)'
+        read custom_nodejs
+    done
+
+    if [ "$custom_nodejs" == 'y' ] || [ "$custom_nodejs" == 'Y' ]  ; then
+        node_version=
+        while [ -z $node_version ]
+        do
+            echo -n 'Please enter a valid version of node (0.10.3)
+    '
+            read node_version
+        done
+        ## probably change this branch name to something other than 'upstream'
+        git remote add upstream -m master git://github.com/openshift/nodejs-custom-version-openshift.git
+        exitStatus $?
+        git pull -s recursive -X theirs upstream master
+        exitStatus $?
+        echo "$node_version" >> .openshift/markers/NODEJS_VERSION
+        exitStatus $?
+        git add .openshift/markers/NODEJS_VERSION
+        exitStatus $?
+        git commit -m 'use custom Node version '
+        exitStatus $?
+    fi
+
+    ## Add Strava.node.js Repo (not sure about the -m flag)
+    git remote add Strava.node.js -m $rhc_name https://github.com/redcap3000/Strava.node.js.git
+    exitStatus $?
+    ## Get 'app' branch from Strava.node.js
+    git pull -s recursive -X theirs Strava.node.js $rhc_name
+    exitStatus $?
+    ## Build JSON ... do this a tad better..
+    if [ "$rhc_name" == 'master' ]; then
+        rhc_domain=
+        while [ -z $rhc_domain ]
+        do
+            echo -n 'rhcloud domain for segment and ride nodes? <http://rides-<AccountName>.rhcloud.com>
+        '
+            read rhc_domain
+        done
+        echo '{   "local":{
+                "ipaddress" : "192.168.0.198",
+                "segment" : "http://192.168.0.198:8081",
+                "rides" : "http://192.168.0.198:8082",
+                "_ports" : {"ipaddress" : 8080, "segment": 8081,"rides":8082},
+                "_timeouts" : {"athlete+rides_offset":3600,"efforts_timeout":25200,"club_segment":3600,"segment":1800}
+            },
+            "openshift":
+            {
+                "_oDomain" : "'$rhc_domain'",
+                "_cDomain" : "rhcloud.com"
+            }
+        }
+        '> nodes.json
+        git add '/nodes.json'
+        exitStatus $?
+    fi
+    
+    git push
+    exitStatus $?
+
+    ## Done??
+    echo '
+    ********
+    Success!
+    ********
+    '
+fi
